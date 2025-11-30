eb127cc66d0cc2326362dbec5eacb25d6d3abdc2
QuachGHuy <quachhuy97@gmail.com>
2025-11-30 08:50:44 +0700

    fix(alembic): configure env.py to support async migrations

--- alembic/env.py
+++ alembic/env.py
@@ -10,6 +10,11 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 
 from app.db.base import Base
+<<<<<<< Updated upstream
+=======
+from app.models import user, apikey
+
+>>>>>>> Stashed changes
 from app.core.config import settings
 
 sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(__file__), '..')))
